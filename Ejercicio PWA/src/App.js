9245ebb02dc22a8c4ce285d98b40a40420a58cd4
Santiago Leal <ds.tenjo@uniandes.edu.co>
2020-11-26 23:10:51 -0500

    ejercicio PWA

---
+++ Ejercicio PWA/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Joke from './Joke'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Joke></Joke>
+    </div>
+  );
+}
+
+export default App;
---
+++ Ejercicio PWA/src/Joke.js
@@ -0,0 +1,56 @@
+import React, {  useState } from "react";
+import { useEffect } from "react";
+import md5 from "md5";
+
+const privK= "f7c2af1bade6016326a191e6077b4f5c79979779";
+const pubk= "cd715021f101344749cee5be334aa073";
+
+
+
+function Joke() {
+    
+    const [joke, setJoke] = useState([]);
+    useEffect(()=>{
+        if(!navigator.onLine){
+            if(localStorage.getItem("joke") === null) {
+                setJoke("Loading...")
+            } else {
+                setJoke(localStorage.getItem("joke"));
+            }
+        } else {
+            const url = new URL("https://gateway.marvel.com/v1/public/characters?");
+            let hashed = md5("Hola"+privK+pubk);
+            let params = {
+                ts:"Hola",
+                hash: hashed,
+                apikey: pubk
+
+            };
+            url.search = new URLSearchParams(params);
+            fetch(url).then(res=>res.json()).then(res=>{
+                setJoke(res.data.results);
+                localStorage.setItem("joke", JSON.stringify(res.data.results));
+            })
+        }
+    }, []);
+
+  
+    
+    return (
+        <div>
+            <h1>Joke</h1>
+          <p>{joke.length} heroes</p>
+          {joke.map((heroe) =>{
+              return(
+                <div ker = {heroe.name}>{heroe.name}</div>
+              );
+          })}
+          
+        </div>
+    );
+    
+  }
+  
+  export default Joke;
+
+
